ae2636a2a2b11d53f4a7511c884855c121109c3a
dikraben <t8sadi00@students.oamk.fi>
2019-11-07 10:19:01 +0200

    database changes

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ const saltRounds = 4;
 app.use(bodyParser.json());
 app.use(cors())
 
-passport.use(new Strategy((username, password, cb) => {
-  db.query('SELECT id, username,email, password FROM users WHERE username = ?', [username]).then(dbResults => {
+passport.use(new Strategy(( email , password, cb) => {
+  db.query('SELECT id, name, email, password FROM users WHERE email = ?', [email]).then(dbResults => {
 
     if(dbResults.length == 0)
     {
@@ -37,7 +37,7 @@ passport.use(new Strategy((username, password, cb) => {
 
 
 app.get('/users', (req, res) => {
-  db.query('SELECT id, username, email FROM users').then(results => {
+  db.query('SELECT id, name, email FROM users').then(results => {
     res.json(results);
   })
 })
@@ -45,22 +45,23 @@ app.get('/users', (req, res) => {
 app.get('/users/:id',
         passport.authenticate('basic', { session: false }),
         (req, res) => {
-          db.query('SELECT id, username, email FROM users WHERE id = ?', [req.params.id]).then(results => {
+          db.query('SELECT id, name, email FROM users WHERE id = ?', [req.params.id]).then(results => {
             res.json(results);
           })
         });
 
 app.post('/users', (req, res) => {
-  let username = req.body.username.trim();
+  let name = req.body.name.trim();
+  let email = req.body.email.trim();
   let password = req.body.password.trim();
 
-  if((typeof username === "string") &&
-     (username.length > 4) &&
+  if((typeof email === "string") &&
+     (email.length > 4) &&
      (typeof password === "string") &&
      (password.length > 6))
   {
     bcrypt.hash(password, saltRounds).then(hash =>
-      db.query('INSERT INTO users (username,email, password) VALUES (?,?)', [username, ,hash])
+      db.query('INSERT INTO users (name, email, password) VALUES (?,?,?)', [name, email ,hash])
     )
     .then(dbResults => {
         console.log(dbResults);
@@ -69,7 +70,7 @@ app.post('/users', (req, res) => {
     .catch(error => res.sendStatus(500));
   }
   else {
-    console.log("incorrect username or password, both must be strings and username more than 4 long and password more than 6 characters long");
+    console.log("incorrect email or password, both must be strings and email more than 4 long and password more than 6 characters long");
     res.sendStatus(400);
   }
 })
@@ -80,7 +81,7 @@ Promise.all(
   [
       db.query(`CREATE TABLE IF NOT EXISTS users(
           id INT AUTO_INCREMENT PRIMARY KEY,
-          username VARCHAR (32),
+          name VARCHAR (32),
           email VARCHAR(32),
           password VARCHAR(256)
       )`)
