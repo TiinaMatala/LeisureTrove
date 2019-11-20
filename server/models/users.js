d1ba7bee22bd0c96c6833957a24db8ff61863f2a
Tiina Matala <t7mati01@students.oamk.fi>
2019-11-20 10:06:36 +0200

    commit before Viivis merge

--- server/models/users.js
+++ server/models/users.js
@@ -11,17 +11,22 @@ var users = {
   getById: function(id, callback) {
     return db.query('select * from users where id=?', [id], callback);
   },
-  add: function( users , callback) {
-    bcrypt.hash(users.password, saltRounds).then(hash =>
-      db.query('INSERT INTO users (name, email, password) VALUES (?,?,?)', [users.name, users.email, hash])
-    )
-    .then(dbResults => {
-        console.log(dbResults);
-        //res.sendStatus(201);
-        
-    })
-    .catch(error => res.sendStatus(500));
-    callback
+  add: function(users, callback) {
+
+    var salt = bcrypt.genSaltSync(10);
+
+    var hash = bcrypt.hashSync(users.password, salt);
+
+      return db.query(
+
+      'insert into users (name, email, password) values(?,?,?)',
+
+      [users.name, users.email, hash],
+
+      callback
+
+    );
+
   },
   delete: function(id, callback) {
     return db.query('delete from users where id=?', [id], callback);
@@ -33,28 +38,11 @@ var users = {
       callback
     );
   },
-  login: function( email , callback) {
-    passport.use(new Strategy((email, password, cb) => {
-      db.query('SELECT id, email, password FROM users WHERE email = ?', [email]).then(dbResults => {
-    
-        if(dbResults.length == 0)
-        {
-          return cb(null, false);
-        }
-    
-        bcrypt.compare(password, dbResults[0].password).then(bcryptResult => {
-          if(bcryptResult == true)
-          {
-            cb(null, dbResults[0]);
-          }
-          else
-          {
-            return cb(null, false);
-          }
-        })
-    
-      }).catch(dbError => cb(err))
-    }));
-  }
+  login: function (email, callback) {
+    return db.query('select password from users where email=?',
+    [users.email, hash],
+    callback
+    );
+   }
 };
 module.exports = users ;
