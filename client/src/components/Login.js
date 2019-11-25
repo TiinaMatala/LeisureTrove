8bde6ef4620f39df200a3b50540be48a264fad35
Tiina Matala <t7mati01@students.oamk.fi>
2019-11-25 13:31:51 +0200

    login function changed to component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,42 +1,88 @@
-import React from 'react';
-import Auth from './Auth';
+import React, { Component } from 'react';
 import styles from './Login.module.css';
 import axios from 'axios';
 
-export default function Login(props) {
+export default class Login extends Component {
 
-    function Cancel(event) {
+  constructor(props) 
+  {
+    super(props);
+
+    this.state = {
+      message: "",
+      email: "",
+      password: ""
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    let myauth=MySingleton.getInstance();
+    this.setState({email:myauth.getUser()});
+  }
+
+  login = event => {
+        let myauth=MySingleton.getInstance();
+        console.log("login");
         event.preventDefault();
-        props.history.goBack();
-    }
-
-    function login(event) {
-        event.preventDefault();
-        props.onSubmit(event);
-        axios.post('http://localhost:4000/users', {
-            email: event.target.email.value,
-            password: event.target.password.value
-        }
-        )
-
-        .then(function (response) {
-            console.log(response);
-            props.history.goBack();
+        const { email, password } = this.state;
+    
+        axios
+          .post('http://localhost:4000/users/login', { email, password},
+            )
+          .then(res => {
+            if(res.data.errno>0) {
+              this.setState({
+                message: "ERROR: "+res.data.sqlMessage
+              });
+            }
+            else {
+              this.componentDidMount();
+            } 
+            if(res.data==false){
+              console.log('un auth');
+              myauth.setAuth('Quest','');
+            }
+            else {
+              console.log('authorized');
+              myauth.setAuth(res.data,this.state.password);
+            }
+            this.setState({email:myauth.getUser()});
           })
-          .catch(function (error) {
-            console.log(error);
-          });
+          .catch(error => {
+            this.setState({
+              message: "ERROR: You have to be administrator to access this option "
+            });
+          })
+      };
+
+  logout(){
+    var myauth=MySingleton.getInstance();
+    myauth.setAuth('Quest',null);
+    this.setState({email:"Quest"});
     }
 
-    return (
-        <div>
-            <div className={ styles.header }>
+  Cancel = event => {
+    event.preventDefault();
+    this.props.history.goBack();
+}
+
+onChange = e => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  };
+
+    render() {
+        return (
+            <div>
+                <div className={ styles.header }>
                 <h1>Header</h1>
             </div>
 
             <h2>Login</h2>
             
-            <form onSubmit={ login }>
+            <form onSubmit={ this.login.bind(this) }>
                 <div className={ styles.loginForm }>
                 
                     <table>
@@ -47,17 +93,17 @@ export default function Login(props) {
                         
                         <tr>
                             <td><label htmlFor="email">Email</label></td>
-                            <td><input name="email" type="text"></input></td>
+                            <td><input name="email" type="text" onChange={this.onChange}></input></td>
                         </tr>
 
                         <tr>
                             <td><label htmlFor="password">Password</label></td>
-                            <td><input name="password" type="password"></input></td>
+                            <td><input name="password" type="password" onChange={this.onChange}></input></td>
                         </tr>
 
                         <tr>
                             <td><button type="submit">Login</button></td>
-                            <td><button onClick={ Cancel }>Cancel</button></td>
+                            <td><button onClick={ this.Cancel.bind(this) }>Cancel</button></td>
                         </tr>
 
                         </tbody>  
@@ -68,6 +114,7 @@ export default function Login(props) {
 
             </form>
             
-        </div>
-    )
+            </div>
+        )
+    }
 }
